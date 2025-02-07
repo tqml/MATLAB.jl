eaea0db04f7c1f7881b9fc2faaf08665096d163f
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2025-02-07 11:26:25 +0100

    Test matlab on Github

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,14 @@ using Test
 
 is_ci() = lowercase(get(ENV, "CI", "false")) == "true"
 
-if !is_ci() # only test if not CI
-    include("engine.jl")
-    include("matfile.jl")
-    include("matstr.jl")
-    include("mxarray.jl")
-end
+# if !is_ci() # only test if not CI
+#     include("engine.jl")
+#     include("matfile.jl")
+#     include("matstr.jl")
+#     include("mxarray.jl")
+# end
+
+include("engine.jl")
+include("matfile.jl")
+include("matstr.jl")
+include("mxarray.jl")
